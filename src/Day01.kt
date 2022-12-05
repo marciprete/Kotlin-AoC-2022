075ee35e5822cdb4908e2fddfd11961397f688a3
Michele <>
2022-12-05 16:30:37 +0100

    Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
+    fun getCalories(input: List<String>): List<Int> {
+        return input.foldIndexed(ArrayList<ArrayList<Int>>(1)) { index, acc, item ->
+            if(index==0 || item.isEmpty()) {
+                acc.add(ArrayList())
+            } else {
+                acc.last().add(item.toInt())
+            }
+            acc
+        }.map { it.sum() }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getCalories(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getCalories(input).sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
